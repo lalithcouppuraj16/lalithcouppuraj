9424c673344d503739a224414482520ef1c63790
Lalith Couppuraj <lalith.couppuraj@saabgroup.com>
2024-03-02 13:21:14 +0000

    Slave address and register address definition

--- i2c.c
+++ i2c.c
@@ -11,6 +11,21 @@
 #include <stdint.h>
 #include <stdio.h>
 
+//Slave addresses and register addresses are referred from the given datasheet
+//Define the magnetometer sensor address or device I2C address 
+#define I2C_ADDRESS 0x1E
+
+//Define the register addresses
+#define CTRL_REG1   0x20     //for output data rate
+#define CTRL_REG2   0x21     //for full scale configuration
+#define INT_CFG     0x30     //for interrupt enable/disable
+#define OUT_X_L     0x28     //for X axis output
+#define OUT_X_H     0x29     //for X axis output
+#define OUT_Y_L     0x2A     //for Y axis output
+#define OUT_Y_H     0x2B     //for Y axis output
+#define OUT_Z_L     0x2C     //for Z axis output
+#define OUT_Z_H     0x2D     //for Z axis output
+
 status_t i2c_read(
     uint8_t bus_address,
     uint8_t register_address,
